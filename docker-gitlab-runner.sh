09df35e20bff345790d0a41c70067a746e43cb1c
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2017-07-13 09:05:38 +0200

    Add runner script

--- docker-gitlab-runner.sh
+++ docker-gitlab-runner.sh
@@ -4,11 +4,19 @@ HEADER="Docker gitlab-runner -"
 
 echo "$HEADER in process..."
 
-docker run -d --name gitlab-runner --restart always \
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo "$DIR"
+
+docker run -d --name gitlab-runner --restart always --privileged \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v ./gitlab-runner/config:/etc/gitlab-runner \
+  -v $DIR/gitlab-runner/config:/etc/gitlab-runner \
   gitlab/gitlab-runner:latest
 
+echo "Recommended executor type: docker | default docker image: alpine:latest"
+
 docker exec -it gitlab-runner gitlab-runner register
+
+echo "$HEADER perform: sudo vim gitlab-runner/config/config.toml and set privileged to true."
 
 echo "$HEADER complete."
