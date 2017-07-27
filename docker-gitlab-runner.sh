aba76af240dc7d3845927fb4d31c5b05845f9e4f
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2017-07-27 17:55:03 +0200

    Update gitlab runner script

--- docker-gitlab-runner.sh
+++ docker-gitlab-runner.sh
@@ -8,37 +8,25 @@
 TOKEN_FROM_GITLAB=$1
 HEADER="Docker gitlab-runner -"
 
-REGISTRY_PORT=5000
-
 echo "$HEADER in process..."
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# docker run.sh --privileged --name some-docker -d docker:dind --insecure-registry 10.10.2.111:5000
-
-#  --insecure-registry $GITLAB_HOST:$REGISTRY_PORT \
 docker run -d --name gitlab-runner --restart always --privileged \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $DIR/certs:/certs \
   -v $DIR/gitlab-runner/config:/etc/gitlab-runner \
+  -v /var/run/docker.sock:/var/run/docker.sock \
   gitlab/gitlab-runner:latest
-
-REGISTRATION_FOLDER=/etc/docker/certs.d/$GITLAB_HOST:$REGISTRY_PORT
-sudo mkdir -p $REGISTRATION_FOLDER
-sudo cp certs/registry-auth.crt $REGISTRATION_FOLDER/ca.crt
 
 echo "Recommended executor type: docker | default docker image: alpine:latest"
 
-# docker exec -it gitlab-runner gitlab-runner register
-
 docker exec -it gitlab-runner gitlab-ci-multi-runner register -n \
-  --url http://$GITLAB_HOST:10080 \
+  --url https://$GITLAB_HOST:443 \
   --registration-token $TOKEN_FROM_GITLAB \
   --executor docker \
   --description "Runner 1" \
   --docker-image "docker:latest" \
   --docker-volumes /var/run/docker.sock:/var/run/docker.sock
 
-echo "$HEADER perform: 's' and set privileged to true and make sure the docker.sock volumes are included = ['/cache', '/var/run.sh/docker.sock:/var/run.sh/docker.sock']."
+echo "$HEADER perform: 'sudo vim $DIR/gitlab-runner/config/config.toml' and set 'privileged' to 'true' and make sure the docker.sock volumes are included = ['/cache', '/var/run.sh/docker.sock:/var/run.sh/docker.sock']."
 
 echo "$HEADER complete."
